602f314970f1d5ce44ba5d7571e619311e88698b
caherfe <caherfe@hotmail.com>
2013-12-05 12:44:34 +0100

    fdas

--- src/com/trabajo/agenda/MainActivity.java
+++ src/com/trabajo/agenda/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		//Hola carmen
+		//Hola de nuevo, Miguel, esta vez no elimino la l’nea
 	}
 
 	@Override
