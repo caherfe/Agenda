999acf256a2255209e4af8f57f8aa72a8f8f1720
caherfe <caherfe@hotmail.com>
2013-12-07 02:05:58 +0100

    kdjflsa

--- src/com/trabajo/agenda/MainActivity.java
+++ src/com/trabajo/agenda/MainActivity.java
@@ -1,17 +1,44 @@
 package com.trabajo.agenda;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private ListView lstContactos;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		//Hola carmen
-		//Hola de nuevo, Miguel, esta vez no elimino la l’nea
+		
+		//Obtenci—n de referencias java
+		lstContactos= (ListView)this.findViewById(R.id.lstContactos);
+		
+		//Base de datos
+		ContactosSQLiteHelper bdConexion = new ContactosSQLiteHelper(this);
+		bdConexion.abrir();
+		
+		/*for(int i=1; i<=10; i++){
+			bdConexion.insertarContacto("Nombre "+i, "Apellidos "+i);
+		}*/
+		
+		Cursor cursor = null;
+		cursor= bdConexion.obtenerContactos();
+		SimpleCursorAdapter adaptador = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cursor, new String[]{ContactosSQLiteHelper.ID_NOMBRE}, new int[]{android.R.id.text1},0);
+	    lstContactos.setAdapter(adaptador);
+		
+	    bdConexion.cerrar();
+		
+		
 	}
 
 	@Override
